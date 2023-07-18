9bc3add9d79b40d35340c98e355b156a153793ad
Svetlana <svetapav@yandex.ru>
2023-07-18 17:04:53 +0300

    доработка роутинга

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -5,20 +5,12 @@ import cn from "classnames"
 import { format } from 'date-fns'
 import { useContext } from "react"
 import { FirstLevelMenuItem, PageItem } from "@/interfaces/menu.interface"
-import CoursesIcon from './icons/courses.svg'
-import BooksIcon from './icons/books.svg'
-import ProductsIcon from './icons/products.svg'
-import ServicesIcon from './icons/services.svg'
-import { TopLevelCategory } from "@/interfaces/page.interface"
+
 import Link from "next/link"
 import { useRouter } from "next/router"
+import { firstLevelMenu } from "../../helpers/helpers"
 
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products }
-];
+
 
 export const Menu = (): JSX.Element => {
 	const { menu, firstCategory, setMenu } = useContext(AppContext);
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,8 +1,6 @@
-import { GetStaticProps } from 'next';
+
 import React from 'react';
 import { withLayout } from '../layout/Layout';
-import axios from 'axios';
-import { MenuItem } from '../interfaces/menu.interface';
 
 
 function Search(): JSX.Element {
