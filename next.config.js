7001689b68d063fe5e303f1dba74448f9e7e1217
Svetlana <svetapav@yandex.ru>
2023-08-03 19:49:21 +0300

    using Next Image component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  images: { 
+    domains: ['courses-top.ru']
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
