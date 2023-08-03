fbe502eb8fd24fdb104c94d66450179d3e98cc2d
Svetlana <svetapav@yandex.ru>
2023-08-03 16:41:30 +0300

    добавление характеристик

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -33,7 +33,15 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 			<div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
 			<Divider className={styles.hr}/>
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>фичи</div>
+			<div className={styles.feature}>
+				{product.characteristics.map(c => (
+					<div className={styles.characteristics} key={c.name}>
+						<span className={styles.characteristicsName}>{c.name}</span>
+						<span className={styles.characteristicsDots}></span>
+						<span className={styles.characteristicsValue}>{c.value}</span>
+					</div>
+				))}
+			</div>
 			<div className={styles.advBlock}>
 				{product.advantages && <div className={styles.advantages}>
 					<div className={styles.advTitle}>Преимущества</div>
