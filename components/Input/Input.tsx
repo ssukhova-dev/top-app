a0410e13678669d863b1f6dd1a2e019a1a89193b
Svetlana <svetapav@yandex.ru>
2023-08-08 14:16:50 +0300

    отображение офибок для контролов формы

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -6,9 +6,7 @@ import { ForwardedRef, forwardRef } from 'react';
 export const Input = forwardRef(({ className, error, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
 	return (
 		<div className={cn(className, styles.inputWrapper)}>
-			<input className={cn(styles.input, {
-				[styles.error]: error
-			})} ref={ref} {...props} />
+			<input className={cn(styles.input, {[styles.error]: error})} ref={ref} {...props} />
 			{error && <span role="alert" className={styles.errorMessage}>{error.message}</span>}
 		</div>
 	);
