c9985b7048311a4180256b1ade1820fa38d09330
Svetlana <svetapav@yandex.ru>
2023-08-10 17:04:07 +0300

    отправка запроса на сохранение отзыва

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,10 @@
 
 import React from 'react';
 import { withLayout } from '../layout/Layout';
-
+import { GetStaticProps } from 'next'
+import axios from 'axios'
+import { MenuItem } from '@/interfaces/menu.interface'
+import { API } from '@/helpers/api';
 
 function Search(): JSX.Element {
 
@@ -13,4 +16,22 @@ function Search(): JSX.Element {
 }
 
 export default withLayout(Search);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
 
