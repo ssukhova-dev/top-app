be0e312941dfdb8627087524742be1b3064bc685
Svetlana <svetapav@yandex.ru>
2023-08-14 15:41:25 +0300

    fix - не перезагружались продукты

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -4,7 +4,7 @@ import cn from "classnames"
 import { Advantages, HhData, Htag, P, Product, Sort, Tag } from "@/components";
 import { TopLevelCategory } from "@/interfaces/page.interface";
 import { SortEnum } from "@/components/Sort/Sort.props";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { sortReducer } from "./sort.reducer";
 
 
@@ -14,6 +14,10 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 	const setSort = (sort: SortEnum) => {
 		dispatchSort({type: sort});
 	};
+
+	useEffect(() => {
+		dispatchSort({ type: 'reset', initialState: products});
+	}, [products]);
 
 	return (
 		<div>
