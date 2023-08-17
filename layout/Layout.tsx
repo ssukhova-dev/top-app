040136cd5978edcc581ab970ae6a83866e53a6b9
Svetlana <svetapav@yandex.ru>
2023-08-17 16:38:50 +0300

    btn Up внизу страницы с анимацией

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -6,6 +6,7 @@ import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
 import { FunctionComponent } from "react";
 import { AppContextProvider } from "../context/app.context";
+import { Up } from "@/components";
 
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
@@ -17,6 +18,7 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
 				{children}
 			</div>
 			<Footer className={styles.footer} />
+			<Up/>
 		</div>
 	);
 }
