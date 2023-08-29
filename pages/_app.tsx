d4650c1478a0c5721332305bba001df8659db481
Svetlana <svetapav@yandex.ru>
2023-08-29 15:46:07 +0300

    установка yndex метрики

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,33 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import ym from 'react-yandex-metrika'
+import {YMInitializer} from 'react-yandex-metrika'
 
 export default function App({ Component, pageProps, router}: AppProps): JSX.Element {
+
+  router.events.on('routeChangeComplete', (url: string) => {
+      if (typeof window !== 'undefined') {
+        ym('hit', url);
+      }
+  });
+
   return <>
 
     <Head>
       <title>My top</title>
       <link rel="icon" href="./favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://mc.yandex.ru" />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;400;500;700&display=swap" rel="stylesheet" />
       <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
       <meta property="og:locale" content="ru_RU" />
     </Head>
-
+    <YMInitializer
+    accounts={[]}
+    options={{ webvisor: true, defer: true}}
+    version="2"
+    />
 
     <Component {...pageProps} >
 
