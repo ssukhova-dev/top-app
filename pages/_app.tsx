0fb54c3e5eb06f3ff5c7969d86982f019c0270fc
Svetlana <svetapav@yandex.ru>
2023-08-29 15:13:45 +0300

    установка OpenGraph tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({ Component, pageProps, router}: AppProps): JSX.Element {
   return <>
 
     <Head>
@@ -10,6 +10,8 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <link rel="icon" href="./favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;400;500;700&display=swap" rel="stylesheet" />
+      <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+      <meta property="og:locale" content="ru_RU" />
     </Head>
 
 
